ac8b37b899edd2f0e7f33b57f8e1aa801e446b0a
MohammadRabea <112767251+MohammadRabe@users.noreply.github.com>
2025-05-03 20:32:39 +0300

    Create AsyncDataFetching.cs

---
+++ Week-5/AsyncDataFetching.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+internal class Program
+{
+    public static async Task Main(string[] args)
+      {
+        Console.WriteLine("Fetch Data");
+        Task<int> fetchTask = FetchData();
+        int data = await fetchTask;
+        Console.WriteLine($"Data => {data}");
+    }
+    static async Task<int> FetchData()
+    {
+        await Task.Delay(3000);
+        return 100;
+    }
+    
+
+    
+
+}  
